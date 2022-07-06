38e965523187f7437981a9bbeaae6ca9b75819b9
saad <>
2022-07-06 16:32:29 +0800

    pushed the translation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useTranslation } from "react-i18next";
+import { Button, StyleSheet, Text, View } from "react-native";
+import "./translation";
+import i18n from "./translation";
 
 export default function App() {
+  const { t } = useTranslation();
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>{t("welcome")}</Text>
+      <Button
+        title="Change language to BAHASA MALAYSIA"
+        onPress={() => i18n.changeLanguage("bm")}
+      />
+      <Button
+        title="Change language to ENGLISH"
+        onPress={() => i18n.changeLanguage("en")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
